01e1f18208d2a365b4d32b46da290546957e55f0
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-08 03:04:56 +0600

    4_Admob Interstitial Ads - Android Development with Java - Rakibul Islam - Bohubrihi
    https://developers.google.com/admob/android/interstitial

--- app/src/main/java/com/sujon/admob/MainActivity.java
+++ app/src/main/java/com/sujon/admob/MainActivity.java
@@ -1,16 +1,21 @@
 package com.sujon.admob;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.*;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     AdView adView;
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +28,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        adView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
+
+//        mInterstitialAd.setAdListener(new AdListener() {
+//            @Override
+//            public void onAdClosed() {
+//
+//                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+//            }
+//
+//        });
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, new InterstitialAdLoadCallback() {
+            @Override
+            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                // The mInterstitialAd reference will be null until
+                // an ad is loaded.
+                mInterstitialAd = interstitialAd;
+                Log.i("TAG", "onAdLoaded");
+//                Toast.makeText(this,"Not Loaded",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                // Handle the error
+                Log.i("TAG", loadAdError.getMessage());
+                mInterstitialAd = null;
+            }
+        });
+
+        adView = findViewById(R.id.adView);
+//        AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
+
+    }
+
+    public void OnButtonClick(View view){
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show(MainActivity.this);
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.");
+            Toast.makeText(this,"Not Loaded",Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
